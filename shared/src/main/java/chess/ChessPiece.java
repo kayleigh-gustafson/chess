7a89f070580defef622bfc4eb122e783b6493e66
Kayleigh Gustafson <92392059+kayleigh-gustafson@users.noreply.github.com>
2025-04-30 17:10:00 -0600

    Started implementing moves

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -9,8 +9,9 @@ import java.util.Collection;
  * signature of the existing methods.
  */
 public class ChessPiece {
-
+    ChessPiece.PieceType type;
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+        this.type = type;
     }
 
     /**
@@ -36,7 +37,7 @@ public class ChessPiece {
      * @return which type of chess piece this piece is
      */
     public PieceType getPieceType() {
-        throw new RuntimeException("Not implemented");
+        return type;
     }
 
     /**
@@ -47,6 +48,8 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
+
+        System.out.println(type);
         throw new RuntimeException("Not implemented");
     }
 }
